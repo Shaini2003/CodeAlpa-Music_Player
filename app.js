51e13968205b58b8a347cfaee0efdb823ed85ca4
Shaini2003 <shinitharushika2020@gmail.com>
2024-09-28 23:25:03 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+const songs = [
+  {title: 'Despacito',
+    description: 'Song by Luis Fonsi ft.Daddy Yankee',
+    src: 'despacito.mp3'
+    
+  },
+  {
+    title: 'Let Me Down Slowly',
+    description:'Song by Alec Benjamin',
+    src: 'let me down slowly.mp3'
+  },
+  {
+    title: 'Let Me Love You',
+    description:'Song by Justin Bieber ft.DJ Snake',
+    src: 'let me love you.mp3'
+    
+  },
+  {
+    title: 'Faded',
+    description:'Song by Alan Walker',
+    src: 'faded.mp3'
+  },
+  {
+    title: 'Style',
+    description:'Song by Taylor Swift',
+    src: 'style.mp3'
+  }
+];
+
